4355d999b615bab99eff244463eccc86da5cbd27
CesarCR12 <csarroyo82@gmail.com>
2024-02-13 15:36:15 -0600

    Tienda_Zapatos
    EA-08-Practicas-2

---
+++ Tienda_Zapatos/src/main/java/com/example/Tienda_Zapatos/TiendaZapatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tienda_Zapatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaZapatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaZapatosApplication.class, args);
+	}
+
+}
---
+++ Tienda_Zapatos/src/test/java/com/example/Tienda_Zapatos/TiendaZapatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tienda_Zapatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaZapatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
